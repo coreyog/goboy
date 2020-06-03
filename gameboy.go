fba2c60fa20aa91b06970af0558ec0587a0bc895
Unknown <coreyog@gmail.com>
2020-06-03 14:06:37 -0600

    Corrected Flag Masks and documented them better, finished implementing XOR A

--- gameboy.go
+++ gameboy.go
@@ -21,15 +21,10 @@ type GameBoy struct {
 }
 
 const (
-	// zero flag mask - set if the result of the operation is zero
-	MaskZeroFlag uint8 = 128
-
-	// operation flag mask - set if the operation was subtraction
-	MaskOpFlag uint8 = 64
-
-	// high carry flag mask - set if there was an overflow in the lower half of the result
-	MaskHighCarryFlag uint8 = 32
-
-	// carry flag mask - set if there was an overflow in the result
-	MaskCarryFlag uint8 = 16
+	MaskSignFlag           uint8 = 0b1000_0000 // sign flag mask - set to the state of the most-significant bit of the Accumulator (bit 7)
+	MaskZeroFlag           uint8 = 0b0100_0000 // zero flag mask - set if the result of the operation is zero
+	MaskHighCarryFlag      uint8 = 0b0001_0000 // high carry flag mask - set if there was an overflow in the lower half of the result
+	MaskParityOverflowFlag uint8 = 0b0000_0100 // parity / overflow mask - set if a math operation overflows for most operations, for some operations this acts as a parity bit (1 if result has an even number of 1s, 0 if result has an odd number of 1s)
+	MaskSubtractionFlag    uint8 = 0b0000_0010 // operation flag mask - set if the operation was subtraction
+	MaskCarryFlag          uint8 = 0b0000_0001 // carry flag mask - set if there was an overflow in the result
 )
--- operations.go
+++ operations.go
@@ -1,5 +1,9 @@
 package goboy
 
+import (
+	"math/bits"
+)
+
 func ldsp(gb *GameBoy, displacement byte, immediate uint16) {
 	// LD SP, $FFFF
 	gb.sp = immediate
@@ -7,6 +11,30 @@ func ldsp(gb *GameBoy, displacement byte, immediate uint16) {
 }
 
 func xora(gb *GameBoy, displacement byte, immediate uint16) {
+	// XOR A
 	gb.a ^= gb.a
-	// some flags to set, unclear
+
+	// set sign flag
+	if gb.a&0b1000_0000 != 0 {
+		gb.f |= MaskSignFlag
+	} else {
+		gb.f &= ^MaskSignFlag
+	}
+
+	// set zero flag
+	if gb.a == 0 {
+		gb.f |= MaskZeroFlag
+	} else {
+		gb.f &= ^MaskZeroFlag
+	}
+
+	// set parity flag
+	if bits.OnesCount8(gb.a)%2 == 0 {
+		gb.f |= MaskParityOverflowFlag
+	} else {
+		gb.f &= ^MaskParityOverflowFlag
+	}
+
+	// clear carry, high carry, and subtraction flags
+	gb.f &= ^(MaskHighCarryFlag | MaskSubtractionFlag | MaskCarryFlag)
 }
