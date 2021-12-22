0b6e834fc0e387680ea3243b7979bd9e12ec68a4
Corey Ogburn <coreyog@gmail.com>
2021-12-22 13:27:17 -0700

    Tested build scripts on mac, needed tweaking
    Running `go generate ./...` on a Mac resulted in "GOOS=js is an unrecognized command" and also tried to run the script in the windows file.
    Added proper  build tags to the windows file and Mac stopped trying to run it.
    Changed up how Mac/Linux builds by opening a shell and that shell parsing the command correctly (exact same approach as windows).
    
    Need to check on my linux laptop next...

--- cmd/goboy-wasm/build-other.go
+++ cmd/goboy-wasm/build-other.go
@@ -3,4 +3,4 @@
 
 package main
 
-//go:generate GOOS=js GOARCH=wasm go build -o dist/goboy.wasm .
+//go:generate $SHELL -c "GOOS=js GOARCH=wasm go build -o dist/goboy.wasm ."
--- cmd/goboy-wasm/build-windows.go
+++ cmd/goboy-wasm/build-windows.go
@@ -1,4 +1,6 @@
+//go:build windows
+// +build windows
+
 package main
 
-// no need for a build tag, the filename limits this to windows
 //go:generate cmd /C "set GOOS=js&& set GOARCH=wasm&& go build -o dist/goboy.wasm ."
